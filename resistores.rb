ce19f334e7f9c060bd17db154419c112e4788346
Davi Azevedo <47645089+daviaze@users.noreply.github.com>
2019-09-23 14:43:53 -0300

    Resistores_Hash

--- resistores.rb
+++ resistores.rb
@@ -2,4 +2,3 @@ def resistores cores
     valores = {"Preto" => 0, "Marrom" => 1, "Vermelho" => 2, "Laranja" => 3, "Amarelo" => 4, "Verde" => 5, "Azul" => 6, "Violeta" => 7, "Cinza" => 8, "Branco" => 9}
     return cores.map {|cor| cor = valores["#{cor}"]}.join
 end
-print resistores ["Amarelo", "Verde"]
